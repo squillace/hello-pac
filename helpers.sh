fd76b0d2d428e1b26d188edfd8b943b3c8b4013d
ralph squillace <ralph@squillace.com>
2021-07-20 16:10:31 -0400

    v0.1.3 and works cycling with random env id

--- helpers.sh
+++ helpers.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
+#set -euo pipefail
 
-install() {
-
+random-string() {
+  # remove the damned space
+        cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-8} | head -n 1 | tr '[:upper:]' '[:lower:]'
 }
 
+install() {
+  echo Hello World
+}
 upgrade() {
   echo World 2.0
 }
